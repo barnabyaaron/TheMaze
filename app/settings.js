173643cd5580b82931aa0d5a9f394a6638b90efe
Aaron Barnaby <barnabyaaron@gmail.com>
2017-02-06 23:40:15 +0000

    Added all CORE assets and files.
    Started index
    Created Settings class.

---
+++ app/settings.js
@@ -0,0 +1,23 @@
+﻿Class.subclass('Settings', {
+    get: function (key) {
+        var json = localStorage.getItem(key);
+        if (json === null || json === undefined) {
+            return null;
+        } else {
+            return $.evalJSON(json);
+        }
+    },
+
+    set: function (key, value) {
+        var json = $.toJSON(val);
+        localStorage.setItem(key, json);
+    },
+
+    delete: function (key) {
+        localStorage.removeItem(key);
+    },
+
+    deleteAll: function () {
+        localStorage.clear();
+    }
+}, {});
