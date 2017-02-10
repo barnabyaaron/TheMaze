37853c4bceab09f90b11b9facb0de89348c93d45
barnaba <aaron.barnaby@whsmith.co.uk>
2017-02-10 15:23:23 +0000

    Added some sample map elements. that render. (no collision yet)

--- app/app.js
+++ app/app.js
@@ -1,129 +1,100 @@
 ﻿Class.subclass('App',
 {
-    ASSETS: {
-        "sprites": {
-            "assets/images/archie_64.png": {
-                "tile": 64,
-                "tileh": 64,
-                "map": {
-                    "archie": [0, 2]
-                }
-            },
-            "assets/images/lily_64.png": {
-                "tile": 64,
-                "tileh": 64,
-                "map": {
-                    "lily": [0, 2]
-                }
-            }
-        }
-    },
+    TESTING_MODE: false,
+    TESTING_GRID: false,
+    CHEAT_MODE: false,
 
+    LOG_ELEMENT: null,
+
+    /**********************/
+    STAGE_WIDTH: 16,
+    STAGE_HEIGHT: 16,
+    TILE_SIZE: 32,
+    
     start: function () {
         Class.classesLoaded();
 
         var app = new App();
+        app.setup();
 
-        Crafty.load(App.ASSETS, function () {
-            // When Loaded
-            app.setup();
-        }, function (e) {
-            // Progress   
-        }, function (e) {
-            // Error Loading   
-        });
+        return app;
+    },
+
+    log: function(message) {
+        if (App.TESTING_MODE) {
+            if (App.LOG_ELEMENT === null) {
+                var element = document.createElement('div');
+                element.id = 'log';
+                element.innerHTML = "<br />";
+                document.body.appendChild(element);
+
+                App.LOG_ELEMENT = element;
+            }
+
+            App.LOG_ELEMENT.innerHTML += message + "<br />";
+        }
+    },
+
+    clearLog: function() {
+        if (App.TESTING_MODE) {
+            document.getElementById('log').innerHTML = "";
+        }
     }
 }, {
     setup: function () {
-        var WIDTH = 50;
-        var HEIGHT = 50;
-        var TILE_SIZE = 16;
+        Crafty.init(App.STAGE_WIDTH * App.TILE_SIZE, App.STAGE_HEIGHT * App.TILE_SIZE);
 
-        Crafty.init(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
-        Crafty.scene("Load", function () {
-            Crafty.background("#000");
-            Crafty.e("2D, DOM, Text").attr({ w: WIDTH*TILE_SIZE, h: 20*TILE_SIZE, x: 0, y: HEIGHT / 2 })
-                    .text("Loading...")
-                    .css({ "text-align": "center", "color": "white" });
+        this.setupGame();
+        this.setupAssets();
 
-            //Preload sprites first
-            Crafty.load(App.ASSETS, function () {
-                // When Loaded
-                Crafty.scene("Main");
-            }, function (e) {
-                // Progress   
-            }, function (e) {
-                // Error Loading   
+        this.browserFix();
+
+        this.loadAssets();
+        this.loadLevels();
+    },
+
+    loadAssets: function () {
+        Crafty.scene('loading',
+            function() {
+                Crafty.background('rgb(30,30,30)');
+                Crafty.e('2D, DOM, Text')
+                    .attr({ w: 480, h: 20, x: 0, y: 20 })
+                    .text('Loading...')
+                    .css({ 'text-align': 'center' });
+
+                Crafty.load(Assets.FILES, function() {
+                        // On Load Complete
+                        Crafty.scene('intro');
+                    }, function (e) {
+                        // Progress
+                    }, function(e) {
+                        // Error  
+                    });
             });
-        });
-        Crafty.scene("Load");
+        Crafty.scene('loading');
+    },
 
-        Crafty.scene("Main", function () {
-            archie = Crafty.e("2D, Canvas, lily, Multiway, SpriteAnimation, Collision")
-			    .attr({ x: 160, y: 220, z: 1 })
-                .reel("walk_left", 1000, 0, 9, 9)
-                .reel("walk_right", 1000, 0, 11, 9)
-			    .reel("walk_up", 1000, 0, 8, 9)
-			    .reel("walk_down", 1000, 0, 10, 9)
-                .multiway(24, { W: -90, S: 90, D: 0, A: 180 })
-                .bind('EnterFrame', function () {
-                    if (this.isDown("LEFT_ARROW") || this.isDown("A")) {
-                        if (!this.isPlaying("walk_left")) {
-                            this.animate("walk_left", -1);
-                        }
-                    } else if (this.isDown("RIGHT_ARROW") || this.isDown("D")) {
-                        if (!this.isPlaying("walk_right")) {
-                            this.animate("walk_right", -1);
-                        }
-                    } else if (this.isDown("UP_ARROW") || this.isDown("W")) {
-                        if (!this.isPlaying("walk_up")) {
-                            this.animate("walk_up", -1);
-                        }
-                    } else if (this.isDown("DOWN_ARROW") || this.isDown("S")) {
-                        if (!this.isPlaying("walk_down")) {
-                            this.animate("walk_down", -1);
-                        }
-                    } else {
-                        if (this.isPlaying()) {
-                            this.pauseAnimation();
-                            this.resetAnimation();
-                        }
-                    }
-                })
-                .collision()
+    loadLevels: function () {
+        this.levels = new Levels();
+        this.levels.loadScenes();
+    },
 
-            archie = Crafty.e("2D, Canvas, archie, Multiway, SpriteAnimation, Collision")
-			    .attr({ x: 160, y: 144, z: 1 })
-                .reel("walk_left", 1000, 0, 9, 9)
-                .reel("walk_right", 1000, 0, 11, 9)
-			    .reel("walk_up", 1000, 0, 8, 9)
-			    .reel("walk_down", 1000, 0, 10, 9)
-                .multiway(24, { W: -90, S: 90, D: 0, A: 180 })
-                .bind('EnterFrame', function () {
-                    if (this.isDown("LEFT_ARROW") || this.isDown("A")) {
-                        if (!this.isPlaying("walk_left")) {
-                            this.animate("walk_left", -1);
-                        }
-                    } else if (this.isDown("RIGHT_ARROW") || this.isDown("D")) {
-                        if (!this.isPlaying("walk_right")) {
-                            this.animate("walk_right", -1);
-                        }
-                    } else if (this.isDown("UP_ARROW") || this.isDown("W")) {
-                        if (!this.isPlaying("walk_up")) {
-                            this.animate("walk_up", -1);
-                        }
-                    } else if (this.isDown("DOWN_ARROW") || this.isDown("S")) {
-                        if (!this.isPlaying("walk_down")) {
-                            this.animate("walk_down", -1);
-                        }
-                    } else {
-                        if (this.isPlaying()) {
-                            this.pauseAnimation();
-                            this.resetAnimation();
-                        }
-                    }
-                })
-                .collision()
-        });
+    setupGame: function() {
+        this.game = new Game();
+    },
+
+    setupAssets: function() {
+        this.assets = new Assets();
+    },
+
+    browserFix: function() {
+        //firefox does not support css' zoom, so I had to use scale in it
+        //It behaves a bit different than zoom, thus the following adjustments were required
+        var FIREFOX = /Firefox/i.test(navigator.userAgent);
+        if (FIREFOX && !nozoom) {
+            var craftyDiv = document.getElementById("cr-stage");
+            craftyDiv.style.height = '338px';
+            craftyDiv.style.marginTop = '150px';
+        }
     }
 });
